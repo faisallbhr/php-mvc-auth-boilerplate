14440ca3a938260f0d69bdaf40cf37e822c506bd
Faisal Bahari <fbhr1112@gmail.com>
2024-05-18 20:14:56 +0700

    fix: pdo in model

--- app/models/User.php
+++ app/models/User.php
@@ -11,12 +11,6 @@ use utils\DBConnection;
 class User extends Model
 {
     protected string $table = 'users';
-    private PDO $pdo;
-
-    public function __construct()
-    {
-        $this->pdo = DBConnection::getInstance()->getPdo();
-    }
 
     public function getByEmail($email)
     {
@@ -25,9 +19,5 @@ class User extends Model
         $prepare->execute([$email]);
         $result = $prepare->fetch(PDO::FETCH_ASSOC);
         return $result;
-    }
-    public static function isLoggedIn()
-    {
-        return isset($_SESSION['user']);
     }
 }
